3d316b5c406dd5671d33fb583d4efa267fe9b09b
Zachary Tremblay <131798535+zachary-tremblay@users.noreply.github.com>
2026-06-27 21:24:14 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+
+        if h == len(piles):
+            return piles[-1]
+
+        total = 0
+
+        left = 1
+        right = piles[-1]
+        valid = 0
+        while left <= right:
+
+            mid = (left + right) // 2
+            print(mid)
+            time = 0
+            for b in piles:
+                time = 0
+                for b in piles:
+                    time += math.ceil(b / mid)
+            if time <= h:
+                valid = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+            
+        return valid
+            
+
+        
+
+
